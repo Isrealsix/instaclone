a3d47f956815d4baea8db584f354aec60dd6f7de
Isreal6ix <isrealsix@gmail.com>
2023-05-02 07:28:55 +0700

    connected ant design styles

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,8 @@ import { createApp } from 'vue'
 import Antd from 'ant-design-vue';
 import router from './router';
 import App from './App.vue'
-import './style.css'
+import './style.css';
+import 'ant-design-vue/dist/antd.css'
 
 const app = createApp(App);
 app.use(Antd);
