3e91fca7dc056565f79f8d650f349687c4612964
Isreal6ix <isrealsix@gmail.com>
2023-05-05 01:28:05 +0700

    added userBar

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -1,12 +1,19 @@
 <script setup lang="ts">
 import Container from './Container.vue';
-
+import UserBar from './UserBar.vue';
 
 </script>
 <template>
 	<Container>
 		<div class="profile-container">
-			<h1>USERBAR</h1>
+			<UserBar
+				username="krisla"
+				:userInfo="{
+					posts: 4,
+					followers: 121,
+					following: 12
+				}"
+			/>
 			<h1>IMAGEGALLERY</h1>
 		</div>
 	</Container>
@@ -15,7 +22,6 @@ import Container from './Container.vue';
 <style scoped>
 	.profile-container {
 		display: flex;
-		flex-direction: column;
 		align-items: center;
 		padding: 20px 0;
 	}
---
+++ src/components/UserBar.vue
@@ -0,0 +1,43 @@
+<script lang="ts" setup>
+interface IProps {
+	username: string;
+	userInfo: {
+		posts: number;
+		followers: number;
+		following: number;
+	}
+}
+defineProps<IProps>();
+
+</script>
+
+<template>
+	<div class="userbar-container">
+		<div class="top-content">
+			<ATypographyTitle :level="2">{{ username }}</ATypographyTitle>
+		</div>
+		<div class="bottom-content">	
+			<ATypographyTitle :level="5">{{ userInfo.posts }} posts</ATypographyTitle>
+			<ATypographyTitle :level="5">{{ userInfo.followers }} followers</ATypographyTitle>
+			<ATypographyTitle :level="5">{{ userInfo.following }} following</ATypographyTitle>
+		</div>
+	</div>
+</template>
+
+<style scoped>
+.userbar-container {
+	padding-bottom: 75px;
+}
+
+.bottom-content {
+	display: flex;
+	align-items: center;
+}
+
+.bottom-content h5 {
+	margin: 0 !important;
+	padding: 0;
+	margin-right: 30px !important;
+	align-items: center;
+}
+</style>
