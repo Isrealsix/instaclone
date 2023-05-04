a14110a75427c81f0a9b56a1a967af4a8f7bad47
Isreal6ix <isrealsix@gmail.com>
2023-05-04 08:22:06 +0700

    used reactive on 2way input binding

--- src/components/AuthModal.vue
+++ src/components/AuthModal.vue
@@ -1,11 +1,16 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, reactive } from 'vue';
 
 interface IProps {
 	isLogin: boolean
 }
 const props = defineProps<IProps>()
 const visible = ref<boolean>(false);
+const input = reactive({
+	username: '',
+	email: '',
+	password: ''
+});
 const showModal = () => {
 	visible.value = true
 }
@@ -21,9 +26,9 @@ const title = props.isLogin ? 'Login' : 'Signup';
 	<div>
 		<AButton type="primary" @click="showModal" class="btn">{{ title }}</AButton>
 		<AModal v-model:visible="visible" :title="title" @ok="handleOk">
-			<AInput class="input" v-if="!isLogin" v-model:value="value" placeholder="Username">Basic usage</AInput>
-			<AInput class="input" v-model:value="value" placeholder="Email">Basic usage</AInput>
-			<AInput class="input" v-model:value="value" placeholder="Password">Basic usage</AInput>
+			<AInput class="input" v-if="!isLogin" v-model:value="input.username" placeholder="Username">Basic usage</AInput>
+			<AInput class="input" v-model:value="input.email" placeholder="Email">Basic usage</AInput>
+			<AInput class="input" v-model:value="input.password" placeholder="Password">Basic usage</AInput>
 		</AModal>
 	</div>
 </template>
