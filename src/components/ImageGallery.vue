3c621aebdf6e335c0c9786f3fa2d3c9dffca5ad7
Isreal6ix <isrealsix@gmail.com>
2023-05-05 01:36:23 +0700

    created imageGallery component

---
+++ src/components/ImageGallery.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+
+type Posts = {
+	id: string;
+	image: string;
+}
+
+interface IProps {
+	posts: Posts[]
+}
+defineProps<IProps>();
+</script>
+<template>
+	<div class="image-gallery-container">
+		<img v-for="post in posts" :key="post.id" :src="post.image" >
+	</div>
+</template>
+
+<style scoped>
+.image-gallery-container {
+	display: flex;
+	justify-content: left;
+	flex-wrap: wrap;
+}
+
+img {
+	margin: 5px;
+	width: 200px;
+}
+</style>
