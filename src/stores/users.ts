8044d2f787704b83c28dda7f3008b1806f05c28a
Isreal6ix <isrealsix@gmail.com>
2023-05-05 18:25:45 +0700

    handled error sign up cases

--- src/stores/users.ts
+++ src/stores/users.ts
@@ -1,13 +1,40 @@
 import { defineStore } from 'pinia';
-import { ref } from 'vue';
+import { ref, reactive } from 'vue';
 
 export const useUserStore = defineStore('users', () => {
 	const user = ref(null);
+	const response = reactive({
+		message: ''
+	});
 
-	const handleLogin = () => {}
-	const handleSignup = () => {}
-	const handleLogout = () => {}
+	const validateEmail = (email: string) => {
+		return String(email)
+			.toLowerCase()
+			.match(
+				/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+			);
+	};
+	const handleLogin = () => { }
+	
+	const handleSignup = (credentials: any) => {
+		const { email, password, username } = credentials;
+
+		if (password.length < 6) {
+			return response.message = 'Password length is too short'
+		}
+
+		if (username.length < 4) {
+			return response.message = 'Username is too short'
+		}
+
+		if (!validateEmail(email)) {
+			return response.message = 'Email is invalid'
+		}
+	}
+	
+	const handleLogout = () => { }
+	
 	const getUser = () => { }
 	
-	return { handleLogin, handleSignup, handleLogout, getUser }
+	return { handleLogin, response, handleSignup, handleLogout, getUser }
 })
