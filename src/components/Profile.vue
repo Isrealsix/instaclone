c1956265ebf231efc102d034bff36b519f3f06f4
Isreal6ix <isrealsix@gmail.com>
2023-05-10 16:00:19 +0700

    dynamic userInfo

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -2,7 +2,7 @@
 import Container from './Container.vue';
 import ImageGallery from './ImageGallery.vue';
 import UserBar from './UserBar.vue';
-import { ref, onBeforeMount, watch } from 'vue';
+import { ref, onBeforeMount, watch, reactive } from 'vue';
 import { useRoute } from 'vue-router';
 import { supabase } from '../supabase';
 import { useUserStore } from '../stores/users';
@@ -18,14 +18,38 @@ const user = ref<Record<string, string> | null>(null);
 const loading = ref(false);
 const isFollowing = ref(false);
 const route = useRoute();
+const userInfo = reactive({
+	posts: 0,
+	followers: 0,
+	following: 0,
+});
 
 const { username } = route.params;
 function addNewPost(post: IData) {
 	posts.value.unshift(post);
 }
 
+async function fetchFollowersCount() {
+	const { count } = await supabase
+		.from('followers_following')
+		.select('*', {count: 'exact'})
+		.eq('following_id', user.value?.id)
+	return count;
+}
+
+
+async function fetchFollowingCount() {
+	const { count } = await supabase
+		.from('followers_following')
+		.select('*', {count: 'exact'})
+		.eq('follower_id', user.value?.id)
+	return count;
+}
+
+
+
 function updateIsFollowing(follow: boolean) {
-	isFollowing.value = follow
+	isFollowing.value = follow;
 }
 async function fetchData() {
 	loading.value = true;
@@ -47,6 +71,12 @@ async function fetchData() {
 		.eq('owner_id', user.value?.id);
 
 	await fetchIsFollowing();
+
+	const followerCount = await fetchFollowersCount();
+	const followingCount = await fetchFollowingCount();
+	userInfo.followers = followerCount
+	userInfo.following = followingCount
+	userInfo.posts = posts.value.length
 	posts.value = postsData;
 	loading.value = false;
 }
@@ -59,13 +89,14 @@ async function fetchIsFollowing() {
 			.eq('follower_id', userStore.user.id)
 			.eq('following_id', user.value?.id)
 			.single();
-		if (data) isFollowing.value = true
+		if (data) isFollowing.value = true;
 	}
 }
 
+
 watch(userStore.user, () => {
-	fetchIsFollowing()
-})
+	fetchIsFollowing();
+});
 onBeforeMount(() => {
 	fetchData();
 });
@@ -75,11 +106,7 @@ onBeforeMount(() => {
 		<div class="profile-container" v-if="!loading">
 			<UserBar
 				:user="user"
-				:userInfo="{
-					posts: 4,
-					followers: 121,
-					following: 12,
-				}"
+				:userInfo="userInfo"
 				:addNewPost="addNewPost"
 				:isFollowing="isFollowing"
 				:updateIsFollowing="updateIsFollowing"
