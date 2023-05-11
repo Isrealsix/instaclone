8a4b51b59606b3c8e7ad82b322cbbcdb54b27f42
Isreal6ix <isrealsix@gmail.com>
2023-05-11 12:06:11 +0700

    fetched followers

--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -1,11 +1,28 @@
 <script setup lang="ts">
 import Card from './Card.vue';
+import { supabase } from '../supabase';
+import { useUserStore } from '../stores/users';
+import { onBeforeMount } from 'vue';
+
+const userStore = useUserStore();
 
 const post = {
 	id: 1,
 	username: 'keith urban',
 	caption: 'go on',
 	img: 'https://hips.hearstapps.com/hmg-prod/images/keith-urban-gettyimages-1392268289.jpg'
+}
+
+onBeforeMount(() => {
+	fetchData()
+})
+
+async function fetchData() {
+	const response = await supabase.from('followers_following')
+	.select('following_id')
+	.eq('follower_id', userStore.user.id);
+
+	console.log(response, 'of fetchdata in cards');
 }
 </script>
 <template>
